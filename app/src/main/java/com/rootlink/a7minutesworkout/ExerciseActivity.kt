06be0e32e780bd79f3d5af9db531dc94aa432c36
Pradip1010100 <pradip122019@gmail.com>
2025-09-04 17:31:41 +0530

    DV0.0.1 : 7MinutesWorkout with basic Ui and logic

---
+++ app/src/main/java/com/rootlink/a7minutesworkout/ExerciseActivity.kt
@@ -0,0 +1,106 @@
+package com.rootlink.a7minutesworkout
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.rootlink.a7minutesworkout.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+    private var binding : ActivityExerciseBinding? = null
+
+    private var restTimer : CountDownTimer? =  null
+    private var restProgress = 0
+
+    private var exerciseTimer : CountDownTimer? =  null
+    private var exerciseProgress = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+
+        if(supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+        setRestView()
+    }
+
+    private fun setRestView(){
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+
+    private fun setExerciseView(){
+        binding?.flProgressBar?.visibility = View.INVISIBLE
+        binding?.tvTitle?.text = "Exercise Name"
+        binding?.flProgressView?.visibility = View.VISIBLE
+        if(exerciseTimer != null){
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+        setExerciseProgressBar()
+    }
+
+
+    private fun setRestProgressBar(){
+        binding?.progressBar?.progress = restProgress
+        restTimer = object : CountDownTimer(10000,1000){
+            override fun onTick(time: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10-restProgress).toString()
+            }
+
+            override fun onFinish() {
+                setExerciseView()
+            }
+
+        }.start()
+    }
+
+    private fun setExerciseProgressBar(){
+        binding?.progressBarExercise?.progress = exerciseProgress
+        exerciseTimer = object : CountDownTimer(30000,1000){
+            override fun onTick(time: Long) {
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = 30 - exerciseProgress
+                binding?.tvTimerExercise?.text = (30-exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "30 seconds are over, lets go to the rest view",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(restTimer!=null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        if(exerciseTimer!=null){
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+        binding = null
+    }
+}
---
+++ app/src/main/java/com/rootlink/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.rootlink.a7minutesworkout
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rootlink.a7minutesworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding:ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
